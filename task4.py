77f322b060c1bfc80991c9678d8015eb358fa635
Mathew <babsmathew68@gmail.com>
2024-11-10 22:42:00 -0800

    Adding ClassObject

--- task4.py
+++ task4.py
@@ -19,3 +19,47 @@ def two_sum(nums, target):
 print(two_sum([2,7,11,15], 9)) # [0, 1]
 print(two_sum([3,2,4], 6)) # [1, 2]
 print(two_sum([3,3], 6)) # [0, 1]
+
+
+
+# Using Class object
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        num_to_index = {}
+        
+       
+        for i, num in enumerate(nums):
+            complement = target - num 
+            
+           
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            
+    
+            num_to_index[num] = i
+
+solution = Solution()
+
+
+
+
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(f"Test Case 1: {solution.twoSum(nums1, target1)}")  # Output: [0, 1]
+
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(f"Test Case 2: {solution.twoSum(nums2, target2)}")  # Output: [1, 2]
+
+
+nums3 = [3, 3]
+target3 = 6
+print(f"Test Case 3: {solution.twoSum(nums3, target3)}")  # Output: [0, 1]
