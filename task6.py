2c78886ba1d884dd98b2904308dcd8a7655aa2ea
Babatunde Matt <babsmathew68@gmail.com>
2024-11-24 14:04:55 -0800

    task6.py

--- task6.py
+++ task6.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Set to store characters in the current window
-        char_set = set()
-        left = 0  # Left pointer of the sliding window
-        max_length = 0  # To store the maximum length of substring
+        seen = set()
+        i = 0
+        result = 0
 
-        # Iterate through the string with the right pointer
-        for right in range(len(s)):
-            # If character is in the set, we need to shrink the window
-            while s[right] in char_set:
-                # Remove the character at the left pointer and move the left pointer right
-                char_set.remove(s[left])
-                left += 1
-            # Add the current character to the set
-            char_set.add(s[right])
-            # Update the max_length
-            max_length = max(max_length, right - left + 1)
+        for j in range(len(s)):
+            while s[j] in seen:
+                seen.remove(s[i])
+                i += 1
+            seen.add(s[j])
+            result = max(result, j - i + 1)
 
-        return max_length
+        return result
